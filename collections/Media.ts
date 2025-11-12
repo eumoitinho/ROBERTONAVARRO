19d1c20e20de8f06b5cad51115f6176765f2739f
eumoitinho <joao.silva489@academico.ufgd.edu.br>
2025-11-11 21:15:04 -0300

    refactor: Remove staticURL from Media collection configuration
    - Eliminated the staticURL property from the Media collection configuration to streamline the setup and avoid redundancy.
    
    This change enhances the clarity of the Media configuration.

--- collections/Media.ts
+++ collections/Media.ts
@@ -10,7 +10,6 @@ const Media: CollectionConfig = {
     read: () => true,
   },
   upload: {
-    staticURL: '/media',
     staticDir: 'media',
     mimeTypes: ['image/*', 'video/*', 'application/pdf'],
   },
